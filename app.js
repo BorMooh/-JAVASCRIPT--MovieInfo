8ec0dc1755ce4ed162964c1d065f4c9660bae42d
BorMooh <bor.mohoric2@gmail.com>
2021-06-01 22:23:23 +0200

    Basic back-end
    Script can now read from input text and get the movie. No data is still being shown to the visitor.

--- app.js
+++ app.js
@@ -1,2 +1,29 @@
 //http://www.omdbapi.com/
 //http://www.omdbapi.com/?i=tt3896198&apikey=2d217e17
+
+
+
+//Skripta čaka, da je gumb pritisnjen 
+document.getElementById('form-submit').addEventListener("click", () => {
+    console.log("Button has been clicked!");
+    let inputValueLength = document.querySelector('.form-input').value.length;
+    console.log(inputValueLength);
+
+    //Ali je vnos prazen?
+    if(inputValueLength === 0)
+    {
+        console.log("Vnos je prazen!");
+        return;
+    }
+    let inputText = document.querySelector('.form-input').value;
+
+    const api = `http://www.omdbapi.com/?t=${inputText}&apikey=2d217e17`;
+
+
+    fetch(api).then(odgovor => { return odgovor.json();})
+    .then(data => {
+        console.log(data);
+
+        
+    })
+});
