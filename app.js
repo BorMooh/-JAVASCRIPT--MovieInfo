b2da1996085e684c1c756e818833e2e14ab3be59
BorMooh <bor.mohoric2@gmail.com>
2021-06-02 11:12:52 +0200

    Print basic data

--- app.js
+++ app.js
@@ -1,29 +1,52 @@
 //http://www.omdbapi.com/
 //http://www.omdbapi.com/?i=tt3896198&apikey=2d217e17
 
+/*
+    METASCORE,
+    ACTORS,
+    DIRECTOR,
+    BOX OFFICE
+    IMDB_RATING
+    IMDB_VOTES
+*/
+
 
 
 //Skripta čaka, da je gumb pritisnjen 
 document.getElementById('form-submit').addEventListener("click", () => {
-    console.log("Button has been clicked!");
-    let inputValueLength = document.querySelector('.form-input').value.length;
-    console.log(inputValueLength);
-
-    //Ali je vnos prazen?
-    if(inputValueLength === 0)
-    {
-        console.log("Vnos je prazen!");
-        return;
-    }
     let inputText = document.querySelector('.form-input').value;
-
     const api = `http://www.omdbapi.com/?t=${inputText}&apikey=2d217e17`;
+    getAPI(api);
 
-
-    fetch(api).then(odgovor => { return odgovor.json();})
-    .then(data => {
-        console.log(data);
-
-        
-    })
 });
+
+
+
+async function getAPI(url)
+{
+    const response = await fetch(url);
+
+    let data = await response.json();
+    //console.log(data);
+    InsertData(data);
+
+}
+
+function InsertData(data)
+{
+    console.log(data);
+
+
+    let title = document.querySelector('.data-title');
+    let released = document.querySelector('.data-table-released-info');
+    let genre = document.querySelector('.data-table-genre-info');
+    let plot = document.querySelector('.data-table-plot-info');
+    let poster = document.querySelector('.slika');
+
+
+    poster.src = data.Poster;
+    title.textContent = data.Title;
+    released.textContent = data.Released;
+    genre.textContent = data.Genre;
+    plot.textContent = data.Plot;
+}
